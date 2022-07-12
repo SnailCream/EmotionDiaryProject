0713bc3911cf6eff3fc08d9964051720e84c3560
carki <94.nogari@gmail.com>
2022-07-12 17:18:59 +0900

    열거형 활용

--- EmotionDiaryProject/ViewController.swift
+++ EmotionDiaryProject/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 var btnClickCount: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
 
-
-
+enum btnEmotion {
+    case happy, love, like, emb, sad, gloomy, bored, upset, tear //버튼 왼쪽 위에서부터 우로
+}
 
 class ViewController: UIViewController {
     
@@ -24,74 +25,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var upsetLabel: UILabel!
     @IBOutlet weak var tearLabel: UILabel!
     
+    var userRecentClickedButton: btnEmotion = .happy
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    var count1 = 0
-    var count2 = 0
-    var count3 = 0
-    var count4 = 0
-    var count5 = 0
-    var count6 = 0
-    var count7 = 0
-    var count8 = 0
-    var count9 = 0
-    
-    @IBAction func happyBtnClicked(_ sender: UIButton) {
-        count1 = count1 + 1
+    @IBAction func checkRecentClickedButton(_ sender: UIButton) {
         
-        happyLabel.text = "행복해 \(count1)"
+        if sender.tag == 0 {
+            userRecentClickedButton = .happy
+            btnClickCount[0] += 1
+            
+            happyLabel.text = "행복해 +\(btnClickCount[0])"
+        }else if sender.tag == 1 {
+            userRecentClickedButton = .love
+            btnClickCount[1] += 1
+            
+            loveLabel.text = "사랑해 +\(btnClickCount[1])"
+        }else if sender.tag == 2 {
+            userRecentClickedButton = .like
+            btnClickCount[2] += 1
+            
+            likeLabel.text = "좋아해 +\(btnClickCount[2])"
+        }else if sender.tag == 3 {
+            userRecentClickedButton = .emb
+            btnClickCount[3] += 1
+            
+            embLabel.text = "당황해 +\(btnClickCount[3])"
+        }else if sender.tag == 4 {
+            userRecentClickedButton = .sad
+            btnClickCount[4] += 1
+            
+            sadLabel.text = "속상해 +\(btnClickCount[4])"
+        }else if sender.tag == 5 {
+            userRecentClickedButton = .gloomy
+            btnClickCount[5] += 1
+            
+            blueLabel.text = "우울해 +\(btnClickCount[5])"
+        }else if sender.tag == 6 {
+            userRecentClickedButton = .bored
+            btnClickCount[6] += 1
+            
+            boredLabel.text = "심심해 +\(btnClickCount[6])"
+        }else if sender.tag == 7 {
+            userRecentClickedButton = .upset
+            btnClickCount[7] += 1
+            
+            upsetLabel.text = "궁금해 +\(btnClickCount[7])"
+        }else if sender.tag == 8 {
+            userRecentClickedButton = .tear
+            btnClickCount[8] += 1
+            
+            tearLabel.text = "감동해 +\(btnClickCount[8])"
+        }
+        
     }
     
-    @IBAction func loveBtnClicked(_ sender: UIButton) {
-        count2 = count2 + 1
-        
-        loveLabel.text = "사랑해 \(count2)"
-    }
-    
-    @IBAction func likeBtnClicked(_ sender: UIButton) {
-        count3 = count3 + 1
-        
-        likeLabel.text = "좋아해 \(count3)"
-    }
-    
-    @IBAction func embBtnClicked(_ sender: UIButton) {
-        count4 = count4 + 1
-        
-        embLabel.text = "당황해 \(count4)"
-    }
-    
-    @IBAction func sadBtnClicked(_ sender: UIButton) {
-        count5 = count5 + 1
-        
-        sadLabel.text = "속상해 \(count5)"
-    }
-    
-    @IBAction func blueBtnClicked(_ sender: UIButton) {
-        count6 = count6 + 1
-        
-        blueLabel.text = "우울해 \(count6)"
-    }
-    
-    @IBAction func boredBtnClicked(_ sender: UIButton) {
-        count7 = count7 + 1
-        
-        boredLabel.text = "심심해 \(count7)"
-    }
-    
-    @IBAction func upsetBtnClicked(_ sender: UIButton) {
-        count8 = count8 + 1
-        
-        upsetLabel.text = "궁금해 \(count8)"
-    }
-    
-    @IBAction func tearBtnClicked(_ sender: UIButton) {
-        count9 = count9 + 1
-        
-        tearLabel.text = "감동해 \(count9)"
-    }
     
 }
 
